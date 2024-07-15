f39b34189ad960526f2a42f1f5f2d7a379a7313e
sufyan-rizvi <sofylaiq@gmail.com>
2024-07-15 10:30:08 +0530

    Add project files.

---
+++ Models/Game.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsPlayPig.Models
+{
+    internal class Game
+    {
+        private int _turnNumber;
+        private int _turnScore;
+        private int _totalScore;
+        private int _diceRoll;
+        private string _userReply;
+
+        Random random = new Random();
+
+        public Game()
+        {
+            Console.WriteLine("Let's Play PIG!\n See how many turns it takes you to get to 20.\n Turn ends when you hold or roll a 1.\n" +
+                " If you roll a 1, you lose all points for the turn.\n If you hold, you save all points for the turn.");
+
+            _turnNumber = 1;
+            _totalScore = 0;
+        }
+
+        public int TotalScore
+        {
+            set { _totalScore = value; }
+            get => _totalScore;
+        }
+
+        public int TurnScore
+        {
+            set => _turnScore = value;
+            get => _turnScore;
+        }
+
+        public int TurnNumber
+        {
+            set { _turnNumber = value; }
+            get => _turnNumber;
+        }
+
+        public string UserReply
+        {
+            set => _userReply = value;
+            get => _userReply;
+        }
+
+        public int DiceRollValue
+        {
+            set => _diceRoll = value;
+            get => _diceRoll;
+        }
+
+        public void RollDice()
+        {
+            DiceRollValue = random.Next(1, 6);
+        }
+
+        public void IncrementTurnScore()
+        {
+            TurnScore += DiceRollValue ;
+        }
+
+        public void IncrementTurnNumber()
+        {
+            TurnNumber++;
+        }
+
+        public void IncrementTotalScore()
+        {
+            TotalScore += TurnScore;
+            
+        }
+                  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetsPlayPig.Models;
+
+namespace LetsPlayPig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            Turn(game);
+        }
+
+        static void PrintInsideTurn(Game game)
+        {
+            
+            while ((game.TotalScore + game.TurnScore) <= 20 )
+            {
+
+                Console.WriteLine("\nEnter 'r' to roll again, 'h' to hold.\n");
+
+                game.UserReply = Console.ReadLine();
+
+
+                if (game.UserReply == "r")
+                {
+
+                    game.RollDice();
+
+                    Console.WriteLine($"You Rolled: {game.DiceRollValue}");
+
+                    if (game.DiceRollValue == 1)
+                    {
+                        Console.WriteLine("Turn Over. No Score");
+                        game.IncrementTurnNumber();
+                        game.TotalScore = 0;
+                        game.TurnScore = 0;
+                        break;
+                    }
+                    else
+                    {
+                        game.IncrementTurnScore();
+
+                        Console.WriteLine($"Your turn score is {game.TurnScore} and you total score is {game.TotalScore}");
+
+                        Console.WriteLine($"If you hold, you will have {game.TurnScore + game.TotalScore} points.");
+                    }
+                }
+                else
+                {
+                    game.IncrementTurnNumber();
+                    game.IncrementTotalScore();
+                    Console.WriteLine($"Your turn score is {game.TurnScore} and total score is {game.TotalScore}\n");
+                    game.TurnScore = 0;
+                    break;
+                }
+
+
+
+            }
+        }
+
+        static void Turn(Game game)
+        {
+            while (true)
+            {
+                Console.WriteLine($"\nTurn{game.TurnNumber}\n");
+
+                if (game.TurnNumber == 1 || game.DiceRollValue == 1)
+                {
+                    Console.WriteLine("Welcome to the game of Pig\n");
+                }
+
+                PrintInsideTurn(game);
+
+                if (game.TotalScore + game.TurnScore >= 20)
+                {
+                    Console.WriteLine($"\nYou Win! You finished in {game.TurnNumber} turns\n");
+                    break;
+                }
+            }
+        }
+    }
+}
